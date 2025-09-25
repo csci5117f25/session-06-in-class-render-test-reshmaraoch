ee031f9b63d7b98b8eb42fed25fe25f3bd3230b7
Reshma Rao <raochreshma22@gmail.com>
2025-09-25 08:40:16 -0500

    Commiting updated files

--- server.py
+++ server.py
@@ -1,8 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+guest_list = []
+
+
+@app.route("/", methods=["GET", "POST"])
+
+def index():
+    if request.method == "POST":
+        name = request.form.get("name")
+        message = request.form.get("message")
+
+        if name and message:
+            guest_list.append({"name": name, "message": message})
+
+        
+        return redirect(url_for("index"))
+
+    return render_template("hello.html", guest_list=guest_list)
+
+# @app.route("/<name>")
+# def hello(name):
+#     return render_template("hello.html", name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
