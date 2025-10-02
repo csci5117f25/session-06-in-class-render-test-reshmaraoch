bcde390cb2710bfb68dac027520040b8b7077faa
Reshma Rao <raochreshma22@gmail.com>
2025-10-02 09:14:25 -0500

    Added Auth0

--- server.py
+++ server.py
@@ -1,13 +1,63 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, json, redirect
+import os
 import db
+from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
+app.secret_key = os.environ['FLASK_SECRET']
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=os.environ.get("AUTH0_CLIENT_ID"),
+    client_secret=os.environ.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{os.environ.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
 db.setup()
+
+
+#Auth Code
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token #access to user data before redirecting
+    return redirect(url_for("hello"))
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + os.environ.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("hello", _external=True),
+                "client_id": os.environ.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
 
 @app.route("/")
 @app.route("/<name>")
 def hello(name=None):
-    return render_template("hello.html", name=name, guestbook=db.get_guestbook())
+    if name is not None:
+        print(name)
+        session["name"] = name
+    return render_template("hello.html", name=session.get("name"), guestbook=db.get_guestbook())
 
 
 @app.post("/submit")
